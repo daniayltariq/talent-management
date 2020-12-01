3bc7f1226d256a97a9d9cc6568a7f22704e6518f
wahhabmalik <malik.wahhab@gmail.com>
2020-12-01 15:26:50 +0500

    profile appear on frotend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,7 +62,7 @@ class HomeController extends Controller
             return view('web.pages.models-single',compact('data'));
          }
          else{
-            return view('web.errors.404')->with('text','Please add attachments to your Profile');
+            return view('web.errors.404')->with('text','Profile not found');
          }
       }
       abort(404);
