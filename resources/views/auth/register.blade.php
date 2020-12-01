28c8b107d1e452304d474d32ec027dd0ffda9099
wahhabmalik <malik.wahhab@gmail.com>
2020-12-01 16:20:03 +0500

    profile appear on frotend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -205,7 +205,7 @@ button.btn.btn-default.btn__red.animation.btn-full.pull-right {
                             <div class="form-group">
                                 <label for="country" class="col-sm-4 control-label">Country <span class="req">*</span></label>
                                 <div class="col-sm-8">
-                                    <select name="state" id="state" class="form-control">
+                                    <select name="country" id="country" class="form-control">
                                         <option value="">Select</option>
                                         @foreach ($countries as $country)
                                             <option value="{{$country->nicename}}">{{$country->nicename}}</option>
