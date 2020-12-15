32eafa13f9d0e6f065218d804f7c24b65dad66d5
jawad_ <jawadahmed1024@gmail.com>
2020-12-15 17:41:34 +0500

    ..

--- resources/views/backend/plan/create.blade.php
+++ resources/views/backend/plan/create.blade.php
@@ -31,7 +31,7 @@
             <div class="kt-portlet__head">
                <div class="kt-portlet__head-label">
                   <h3 class="kt-portlet__head-title">
-                     Create Blog
+                     Create Plan
                   </h3>
                </div>
             </div>
