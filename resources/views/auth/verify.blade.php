34f4d8a6faad264616b731e4ade5bab30bd02203
jawad_ <jawadahmed1024@gmail.com>
2020-12-18 19:17:13 +0500

    ..

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -31,10 +31,11 @@
                     @endif
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
+                    <br>
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Resend') }}</button>.
                     </form>
                 </div>
                 </div>
