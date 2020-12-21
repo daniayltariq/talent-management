3261de36c3e8adc627d63cd2a9ef4dd9a8f23993
jawad_ <jawadahmed1024@gmail.com>
2020-12-21 19:50:14 +0500

    ..

--- resources/views/web/components/comments.blade.php
+++ resources/views/web/components/comments.blade.php
@@ -17,25 +17,28 @@
                 </div>
             </div>
             @foreach ($comment->childComment as $child_comment)
-                <ol class="comment-list children">
-                    <li class="comment-list__item">
-                        <div class="comment__body">
-                            <div class="comment__avatar">
-                                <img src="{{ asset('web/img/testimonal-photo.png') }}" alt="">
+                @if ($child_comment->user()->exists())
+                    <ol class="comment-list children">
+                        <li class="comment-list__item">
+                            <div class="comment__body">
+                                <div class="comment__avatar">
+                                    <img src="{{ asset('web/img/testimonal-photo.png') }}" alt="">
+                                </div>
+                                <div class="comment__info">
+                                    <p class="comment__author">{{ $child_comment->user->f_name }} {{ $child_comment->user->l_name }}</p>
+                                    <p class="comment__date date">{{ $child_comment->created_at->diffForHumans() }}</p>
+                                </div>
+                                <div class="comment__content">
+                                    {{ $child_comment->comment }}
+                                </div>
+                                {{-- <div class="comment__reply">
+                                    <button type="button" class="btn btn__grey animation">Reply</button>
+                                </div> --}}
                             </div>
-                            <div class="comment__info">
-                                <p class="comment__author">{{ $child_comment->user->f_name }} {{ $child_comment->user->l_name }}</p>
-                                <p class="comment__date date">{{ $child_comment->created_at->diffForHumans() }}</p>
-                            </div>
-                            <div class="comment__content">
-                                {{ $child_comment->comment }}
-                            </div>
-                            {{-- <div class="comment__reply">
-                                <button type="button" class="btn btn__grey animation">Reply</button>
-                            </div> --}}
-                        </div>
-                    </li>
-                </ol>
+                        </li>
+                    </ol>
+                @endif
+                
             @endforeach
             
         </li>
