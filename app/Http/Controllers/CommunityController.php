cfc36194beeb3018a477f333f10fec080bf87045
jawad_ <jawadahmed1024@gmail.com>
2020-12-22 13:27:31 +0500

    ..

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -121,7 +121,7 @@ class CommunityController extends Controller
     public function read_more_comments(Request $request)
     {
         /* return $request->all(); */
-        $comments = TopicComment::where('topic_id',$request->topic)->where('parent_id',null)->with('childComment')->get()->skip($request->skipcount);
+        $comments = TopicComment::where('topic_id',$request->topic)->where('parent_id',null)->with('childComment')->get()->skip($request->skipcount)->take(1);
         return $comments;
         if($comments){
     		return view('web.components.comments',compact('comments'));
