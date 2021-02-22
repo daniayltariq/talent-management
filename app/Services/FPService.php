b24f6928dda5e2c2dbf57bd9c8e37692d3691d05
Nisar Ahmed <nisar@easoblue.com>
2021-02-22 18:21:45 +0500

    first promoter integrated

--- app/Services/FPService.php
+++ app/Services/FPService.php
@@ -6,19 +6,32 @@ use Illuminate\Support\Facades\Http;
 
 class FPService{
 
+	protected static $base_url_api = 'https://firstpromoter.com/api/v1';
+
 	protected static $api_key = '4a2cda022c723e6e239fdac3adb3dd73';
 
 	public static function trackSigup($email,$ref_id){
 		
 		$response = Http::withHeaders([
 			'x-api-key' => self::$api_key
-		])->post('https://firstpromoter.com/api/v1/track/signup', [
+		])->post(self::$base_url_api.'/track/signup', [
 		    'email' => $email,
 		    'ref_id' => $ref_id,
 		]);
 
-	
+		return $response->body();
+	}
 
+	public static function createPromotor($email,$ref_id = null){
+		
+		$response = Http::withHeaders([
+			'x-api-key' => self::$api_key
+		])->post(self::$base_url_api.'/promoters/create', [
+		    'email' => $email,
+		    'ref_id' => $ref_id,
+		]);
+
+		return $response;
 	}
 
 }
