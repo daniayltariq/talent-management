1068db1da8bf78bfb891a77da3f29376076f25f0
aridiantechs <aridiantechs@users.noreply.github.com>
2021-02-23 15:17:55 +0500

    Download report

--- resources/views/print/resume.blade.php
+++ resources/views/print/resume.blade.php
@@ -7624,8 +7624,8 @@ button.close {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/printThis/1.15.0/printThis.min.js" integrity="sha512-d5Jr3NflEZmFDdFHZtxeJtBzk0eB+kkRXWFQqEc1EKmolXjHm2IKCA7kTvXBNjIYzjXfD5XzIjaaErpkZHCkBg==" crossorigin="anonymous"></script>
 
-    <script src="https://unpkg.com/jspdf@latest/dist/jspdf.min.js"></script>
-    
+    <script src="{{ asset('pdf/js/jspdf1.js') }}"></script>
+
     <script>
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
